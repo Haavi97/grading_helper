4ca8b75c75e47f448a6d6bb1f6335ad1ad058605
Lauri Vihman <lauri.vihman@ttu.ee>
2021-09-27 11:57:18 +0300

    unzipping implemented

---
+++ config.py
@@ -0,0 +1,11 @@
+import os
+
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+
+catkin_ws =  "/home/ubuntu/catkin_ws/src"
+# catkin_ws =  f"{PROJECT_DIR}/catkin_ws"
+homework_bundle_path = f"{PROJECT_DIR}/data"
+results_file = "results.txt"
+supplemental_dir = "supplemental"
+catkin_command = "catkin_make"
+rospack_command = "rospack"
